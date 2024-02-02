87350f41c1fbb08044d53cdfce21ac8f49038b7f
Tim Starling <tstarling@wikimedia.org>
2024-02-02 15:26:39 +0300

    Override LD_LIBRARY_PATH

--- public_html/multires.sh
+++ public_html/multires.sh
@@ -14,8 +14,12 @@ wget -O "$TMP" "https://upload.wikimedia.org/wikipedia/commons/${MD51}/${MD52}/$
 MULTI=${DIR}${MD5}
 rm -rf $MULTI
 
+# Override PYTHONPATH due to buildpack weirdness
+export PYTHONPATH=/layers/fagiani_apt/apt/usr/lib/python3/dist-packages
+
+# Also LD_LIBRARY_PATH due to lack of working ldconfig
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/layers/fagiani_apt/apt/usr/lib/x86_64-linux-gnu/blas:/layers/fagiani_apt/apt/usr/lib/x86_64-linux-gnu/lapack
+
 # generate tiled cube faces
 # Use static nona binary from NFS since hugin-tools is missing from Ubuntu 22.04
-# Override PYTHONPATH due to buildpack weirdness
-PYTHONPATH=/layers/fagiani_apt/apt/usr/lib/python3/dist-packages \
-  ./generate.py --nona="$TOOL_DATA_DIR"/bin/nona -o "$MULTI" "$TMP"
+./generate.py --nona="$TOOL_DATA_DIR"/bin/nona -o "$MULTI" "$TMP"
