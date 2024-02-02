9c58931ee46a66f4ed16b606a8bba225808f4c8c
Tim Starling <tstarling@wikimedia.org>
2024-02-02 14:35:13 +0300

    In generate.sh, specify PYTHONPATH

--- public_html/multires.sh
+++ public_html/multires.sh
@@ -11,8 +11,11 @@ MD51=`echo $MD5 | cut -c1`
 MD52=`echo $MD5 | cut -c1-2`
 wget -O "$TMP" "https://upload.wikimedia.org/wikipedia/commons/${MD51}/${MD52}/${FILE}"
 
-# generate tiled cube faces
-# Use static nona binary from NFS since hugin-tools is missing from Ubuntu 22.04
 MULTI=${DIR}${MD5}
 rm -rf $MULTI
-./generate.py --nona="$TOOL_DATA_DIR"/bin/nona -o "$MULTI" "$TMP"
+
+# generate tiled cube faces
+# Use static nona binary from NFS since hugin-tools is missing from Ubuntu 22.04
+# Override PYTHONPATH due to buildpack weirdness
+PYTHONPATH=/layers/fagiani_apt/apt/usr/lib/python3/dist-packages \
+  ./generate.py --nona="$TOOL_DATA_DIR"/bin/nona -o "$MULTI" "$TMP"
